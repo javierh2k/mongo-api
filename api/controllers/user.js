ff641719223f600a1ed801e91b1323746bf00463
javierh2k <javierh2k@gmail.com>
2019-02-08 09:38:35 -0500

    fix controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,10 +2,6 @@ const UsersCustomRepository  = require('../repositories/user');
 
 class UsersControllers {
   async find(ctx) {
-
-(
-function () {  var foo=2; bar=1}
-)
       ctx.body = await UsersCustomRepository.findPaginate(ctx);
   }
 }
